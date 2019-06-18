b27fc200f6a2e2953b493481c918c461eeb824d9
Anna Albuquerque <anna.b.albuquerque@gmail.com>
2019-06-18 17:26:32 -0300

    Building Initialize: read_file()

--- src/game_loop.cpp
+++ src/game_loop.cpp
@@ -6,6 +6,15 @@ void GameLoop::read_file(){
     std::ifstream file;
     file.exceptions (std::ifstream::badbit);
 
-    std::cout << ">> Trying to open input file (" << inputFile << ")";
+    std::cout << ">> Trying to open input file (" << inputFile << ") ... ";
+    file.open(inputFile); //Opening file in reading mode
 
+    //Checking if the file opened successfully
+    if(!file){
+        std::cerr << "\n\033[0;31m >> Error: opening/reading file\033[0m\n";
+        exit(EXIT_FAILURE);
+    }
+
+    std::cout << "File opened successfully! \n";
+    std::cout << ">> Processing data ... \n";
 }
